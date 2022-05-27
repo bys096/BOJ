e1c64ef880e99459578277a09c5de2f33d471abe
bys096 <jamesban01@gmail.com>
2022-05-28 02:42:35 +0900

    add comment to No.11659

--- No.11659_Summing Up 4.cpp
+++ No.11659_Summing Up 4.cpp
@@ -12,16 +12,16 @@ int main()
     int tmp, l, r;
     vector<int> v(size+1, 0);
 
-    for(int i = 1; i < v.size(); i++) {
+    for(int i = 1; i < v.size(); i++) {     // 누적합으로 배열 생성
         scanf("%d", &tmp);
         sum += tmp;
         v[i] = sum;
     }
 
     for(int i = 0; i < t; i++) {
-        scanf("%d", &l);
-        scanf("%d", &r);
-        printf("%d\n", v[r] - v[l-1]);
+        scanf("%d", &l);                    // 공백기준 왼쪽 수
+        scanf("%d", &r);                    // 공백기준 오른쪽 수
+        printf("%d\n", v[r] - v[l-1]);      // r 위치의 누적합 - l-1위치 까지의 누적합 = 원하는 범위의 누적합
     }
 
     return 0;
